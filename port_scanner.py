d307b9824baa4c41b8e7096758d83d4622647881
tjrink <tjrink@gmail.com>
2025-12-15 10:07:38 +1300

    Added ability to call port scan from UI

---
+++ port_scanner.py
@@ -0,0 +1,23 @@
+import socket
+
+def run_port_scan(min_port, max_port):
+    for i in range(min_port, max_port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.1)
+        try:
+            s.connect(("localhost", i))
+            request = f"GET / HTTP/1.1\r\nHost: {s}\r\nConnection: close\r\n\r\n"
+            s.sendall(request.encode())
+
+            response_bytes = s.recv(1024)
+            response_decoded = response_bytes.decode("UTF-8")
+
+            print (f"Port {i} response: {response_decoded[0:26].rstrip()}")
+            s.close()
+        except:
+            print (f"Port {i} cannot be reached")
+
+
+
+def run(min_port, max_port):
+    run_port_scan(min_port, max_port)
--- ui.py
+++ ui.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import port_scanner
+
+def send_info_to_scanner():
+    min_str = txt_min_port.get()
+    max_str = txt_max_port.get()
+
+    port_scanner.run(int(min_str), int(max_str))
 
 root = tk.Tk()
 root.title("Network Scanner")
@@ -7,17 +14,22 @@ root.state("zoomed")
 frm_min_port = tk.Frame(root, bg="blue")
 
 lbl_min_port = tk.Label(frm_min_port, height=1, text="Min Port")
-txt_min_port = tk.Text(frm_min_port, height=1, width=30)
+txt_min_port = tk.Entry(frm_min_port, width=30)
 lbl_min_port.pack(side="left")
 txt_min_port.pack(side="right")
 
 frm_max_port = tk.Frame(root, bg="red")
 lbl_max_port = tk.Label(frm_max_port, height=1, text="Max Port")
-txt_max_port = tk.Text(frm_max_port, height=1, width=30)
+txt_max_port = tk.Entry(frm_max_port, width=30)
 lbl_max_port.pack(side="left")
 txt_max_port.pack(side="right")
+
+btn_run_scan = tk.Button(root, text="Run Port Scan", command=send_info_to_scanner)
 
 
 frm_min_port.pack()
 frm_max_port.pack()
+btn_run_scan.pack()
 root.mainloop()
+
+

d90753c1b8b478643a12360087a72367c05afd13
tjrink <tjrink@gmail.com>
2025-12-16 08:25:47 +1300

    Created single output from multiple threads

--- port_scanner.py
+++ port_scanner.py
@@ -1,28 +1,42 @@
 import threading
 import socket
 
-def run_port_scan(min_port, max_port, thread):
-    for i in range(min_port, max_port):
-        if(i%2 == thread):
+def run_port_scan(ip_add, min_port, max_port, thread, results_queue):
+    open_ports = set()
+    for i in range(min_port, max_port + 1):
+        if(i%4 == thread):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.1)
+            s.settimeout(0.05)
             try:
-                s.connect(("localhost", i))
+                s.connect((ip_add, i))
                 request = f"GET / HTTP/1.1\r\nHost: {s}\r\nConnection: close\r\n\r\n"
                 s.sendall(request.encode())
 
                 response_bytes = s.recv(1024)
                 response_decoded = response_bytes.decode("UTF-8")
-
-                print (f"Port {i} response: {response_decoded[0:26].rstrip()}")
+                results_queue.add(i)
                 s.close()
             except:
-                print (f"Port {i} cannot be reached")
+                pass
 
 
 
-def run(min_port, max_port):
-    thread_evens = threading.Thread(target=run_port_scan, args=(min_port, max_port, 0))
-    thread_odds = threading.Thread(target=run_port_scan, args=(min_port, max_port, 1))
-    thread_evens.start()
-    thread_odds.start()
+def run(ip_add, min_port, max_port):
+    results_queue = set()
+    thread_1 = threading.Thread(target=run_port_scan, args=(ip_add, min_port, max_port, 0, results_queue))
+    thread_2 = threading.Thread(target=run_port_scan, args=(ip_add, min_port, max_port, 1, results_queue))
+    thread_3 = threading.Thread(target=run_port_scan, args=(ip_add, min_port, max_port, 2, results_queue))
+    thread_4 = threading.Thread(target=run_port_scan, args=(ip_add, min_port, max_port, 3, results_queue))
+
+    thread_1.start()
+    thread_2.start()
+    thread_3.start()
+    thread_4.start()
+
+    thread_1.join()
+    thread_2.join()
+    thread_3.join()
+    thread_4.join()
+
+    return results_queue
+    
--- ui.py
+++ ui.py
@@ -2,23 +2,37 @@ import tkinter as tk
 import port_scanner
 
 def send_info_to_scanner():
+    lbl_results.config(text="Scanning")
+    ip_add = txt_ip.get()
     min_str = txt_min_port.get()
     max_str = txt_max_port.get()
 
-    port_scanner.run(int(min_str), int(max_str))
+    port_set = port_scanner.run(ip_add, int(min_str), int(max_str))
+    results_text = "Open ports: "
+    for port in port_set:
+        results_text+= port + " "
+    
+    lbl_results.config(text=results_text)
 
 root = tk.Tk()
 root.title("Network Scanner")
 root.state("zoomed")
 
-frm_min_port = tk.Frame(root, bg="blue")
+frm_ip_add = tk.Frame(root)
+lbl_ip = tk.Label(frm_ip_add, text="IP Address")
+txt_ip = tk.Entry(frm_ip_add, width=30)
+lbl_ip.pack()
+txt_ip.pack()
+
+
+frm_min_port = tk.Frame(root)
 
 lbl_min_port = tk.Label(frm_min_port, height=1, text="Min Port")
 txt_min_port = tk.Entry(frm_min_port, width=30)
 lbl_min_port.pack(side="left")
 txt_min_port.pack(side="right")
 
-frm_max_port = tk.Frame(root, bg="red")
+frm_max_port = tk.Frame(root)
 lbl_max_port = tk.Label(frm_max_port, height=1, text="Max Port")
 txt_max_port = tk.Entry(frm_max_port, width=30)
 lbl_max_port.pack(side="left")
@@ -26,10 +40,15 @@ txt_max_port.pack(side="right")
 
 btn_run_scan = tk.Button(root, text="Run Port Scan", command=send_info_to_scanner)
 
+frm_results = tk.Frame(root)
+lbl_results = tk.Label(frm_results)
+lbl_results.pack()
 
+frm_ip_add.pack()
 frm_min_port.pack()
 frm_max_port.pack()
 btn_run_scan.pack()
+frm_results.pack()
 root.mainloop()
 
 
